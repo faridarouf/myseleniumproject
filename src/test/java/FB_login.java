6ac63770ca8ef20b76305e5c50c4b699159a038f
farid <farid@LAPTOP-5MLVI5CQ.fios-router.home>
2023-05-02 22:39:35 -0400

    test

--- src/test/java/FB_login.java
+++ src/test/java/FB_login.java
@@ -5,18 +5,17 @@ import org.openqa.selenium.remote.server.handler.SendKeys;
 import org.testng.annotations.Test;
 
 public class FB_login {
-  @Test
-  public void f() {
+	@Test
+	public void f() {
 //create webdriver object
-WebDriver driver;
+		WebDriver driver;
 //open Any browser
-driver=new ChromeDriver();
+		driver = new ChromeDriver();
 //Type FB URL
-driver.get("https://www.facebook.com/");
- driver.findElement(By.id("email")).sendKeys("sonia@gmail.com"); 
-	  
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("sonia@gmail.com");
 
-driver.findElement(By.name("password")).sendKeys("test12322323");
-driver.findElement(By.name("login")).click();	  
-  }
+		driver.findElement(By.name("password")).sendKeys("test12322323");
+		driver.findElement(By.name("login")).click();
+	}
 }
